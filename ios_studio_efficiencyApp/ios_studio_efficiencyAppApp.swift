7243ee74f056e563844d0174c9c273518f6f0e28
Nason <jokerslade76@gmail.com>
2026-05-04 17:06:57 +1000

    Initial Commit

---
+++ ios_studio_efficiencyApp/ios_studio_efficiencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_studio_efficiencyAppApp.swift
+//  ios_studio_efficiencyApp
+//
+//  Created by Nason on 2026/5/4.
+//
+
+import SwiftUI
+
+@main
+struct ios_studio_efficiencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
